4e6b97d691d1d4c29d70ff06eac27d756ad40e22
Gaurav rajbhar <pugbap384@gmail.com>
2022-07-11 22:29:14 +0530

    ceated navbar and compoents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Navs from './Components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
+
+
 function App() {
   return (
 
     <div>
-      hi
+
+      <Navs />
+
+
+
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+
+        </Route>
+
+        <Route >
+          <div>NOT FOUND</div>
+
+        </Route>
+
+      </Switch>
     </div>
 
   );
---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>this is starred</div>
+    )
+}
+
+export default Home;
