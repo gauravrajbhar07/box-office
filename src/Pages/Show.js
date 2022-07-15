b0b43da8b873f94f1739debbb761f23ee67c4bf3
Gaurav rajbhar <pugbap384@gmail.com>
2022-07-15 08:51:01 +0530

    replace useState in place to useReducer

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,17 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from "react-router-dom";
 import { apiGet } from '../misc/config';
+
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+
+        case 'FETCH_SUCCESS': {
+            return { isLoading: false, error: null, show: action.show }
+        }
+
+        case 'FETCH_FAILED': {
+            return { ...prevState, isLoading: false, error: action.error }
+        }
+
+
+        default: return prevState
+    }
+}
+
+const initialState = {
+    show: null,
+    isLoading: true,
+    error: null
+}
+
+
+
 
 const Show = () => {
 
     const { id } = useParams();
-    const [show, setShow] = useState(null);
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
 
 
+    // const [show, setShow] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
+    //now we are using useReducer in place of useState
+
+
+
+
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    console.log(state)
     useEffect(() => {
 
         let isMounted = true;
@@ -21,8 +55,11 @@ const Show = () => {
 
             setTimeout(() => {
                 if (isMounted) {
-                    setShow(result);
-                    setIsLoading(false);
+
+                    dispatch({ type: 'FETCH_SUCCESS', show: result })
+
+                    // setShow(result);
+                    // setIsLoading(false);
 
                 }
 
@@ -31,8 +68,10 @@ const Show = () => {
         }).catch(err => {
             if (isMounted) {
 
-                setError(err.message);
-                setIsLoading(false);
+                dispatch({ type: 'FETCH_FAILED', error: err.message })
+
+                // setError(err.message);
+                // setIsLoading(false);
 
             }
         });
@@ -46,15 +85,15 @@ const Show = () => {
 
     }, [id])
 
-    console.log("show", show)
+    // console.log("show", show)
 
-    if (isLoading) {
-        return <div>Data is being loaded </div>
-    }
+    // if (isLoading) {
+    //     return <div>Data is being loaded </div>
+    // }
 
-    if (error) {
-        return <div> Error occured ": {error}</div>
-    }
+    // if (error) {
+    //     return <div> Error occured ": {error}</div>
+    // }
 
     return (
         <div> this is just a page</div>
