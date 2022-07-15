ad9804f0d64177d7bc17b89cd9f7528db0cf3da9
Gaurav rajbhar <pugbap384@gmail.com>
2022-07-15 07:49:58 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Show from './pages/Show';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
@@ -9,6 +10,11 @@ function App() {
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+
+      <Route exact path="/show/:id">
+        <Show />
+
       </Route>
 
       <Route exact path="/starred">
